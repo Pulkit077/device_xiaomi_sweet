4bcf70f263c3be20b8de1c50f53e17f87afb6048
Sought <sought@tutanota.de>
2022-09-05 09:14:04 +0000

    sweet: libinit: Add missing models
    Signed-off-by: Sought <sought@tutanota.de>

--- libinit/libinit_sweet.cpp
+++ libinit/libinit_sweet.cpp
@@ -23,6 +23,34 @@ static const variant_info_t sweet_global_info = {
     .nfc = true,
 };
 
+static const variant_info_t sweet_eea_info = {
+    .hwc_value = "EEA",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "sweet",
+    .marketname = "Redmi Note 10 Pro",
+    .model = "M2101K6G",
+    .build_fingerprint = "Redmi/sweet_eea/sweet:12/RKQ1.210614.002/V13.0.13.0.SKFEUXM:user/release-keys",
+    .build_description = "sweet_eea-user 12 RKQ1.210614.002 V13.0.13.0.SKFEUXM release-keys",
+
+    .nfc = true,
+};
+
+static const variant_info_t sweet_jp_info = {
+    .hwc_value = "JAPAN",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "sweet",
+    .marketname = "Redmi Note 10 Pro",
+    .model = "M2101K6R",
+    .build_fingerprint = "Redmi/sweet_global/sweet:12/RKQ1.210614.002/V13.0.8.0.SKFMIXM:user/release-keys",
+    .build_description = "sweet_global-user 12 SKQ1.210908.001 V13.0.8.0.SKFMIXM release-keys",
+
+    .nfc = true,
+};
+
 static const variant_info_t sweetin_info = {
     .hwc_value = "INDIA",
     .sku_value = "std",
@@ -53,6 +81,8 @@ static const variant_info_t sweetinpro_info = {
 
 static const std::vector<variant_info_t> variants = {
     sweet_global_info,
+    sweet_eea_info,
+    sweet_jp_info,
     sweetin_info,
     sweetinpro_info,
 };
